066a0e094b98d6e3e79dd7ea7550b1470fe10a4b
Edgar Barrientos <edgarmbarr@gmail.com>
2021-01-05 21:55:56 +0000

    better pokemans quiz

--- pokemans/pokemansRU.py
+++ pokemans/pokemansRU.py
@@ -6,34 +6,40 @@ pokemans = {
         "Charizard": 0,
         "Bulbasaur": 0
         }
-questions = [{
-        "Would you kill ten people to save 1?": {
+questions = [[
+        "Would you kill ten people to save 1?", {
             "Yes": "Pikachu",
             "Depends who the 1 is": "Squirtle",
             "I'll kill 11": "Charizard",
             "I can't chose between lives": "Bulbasaur",
             "I don't want to be involved": "Snorlax"}
-            },
-            {
-        "What is love?": {
+            ],
+            [
+        "What is love?", {
             "How I feel about coffee ;D": "Pikachu",
             "Baby don't hurt me": "Squirtle",
             "I don't even love anything": "Charizard",
             "It's when you deeply care about someone": "Bulbasaur",
             "IDK": "Snorlax"}
-            },
-            {
-        "What is the main reason so many people moved to California in 1849?": {"California land was fertile, plentiful, and inexpensive": "Pikachu",
+            ],
+            [
+        "What is the main reason so many people moved to California in 1849?", {"California land was fertile, plentiful, and inexpensive": "Pikachu",
             "Gold was discovered in central California": "Squirtle",
             "The east was preparing for a civil war":"Charizard",
             "They wanted to establish religious settlements": "Bulbasaur",
             "idk": "Snorlax"}
-        }]
+        ]]
 print("Which pokemon are you quiz:\n")
 num = 0;
 while(num < len(questions)):
     current = questions[num]
-    print(list(current))
+    print(current[0])
+    q = 1
+    for ans in current[1]:
+        print(f"Press {q}: {ans}")
+        q+=1
+    reply = input("Choose number")
+    current = questions[num]
     num+=1
 print("congratz your a pikachu")
     
